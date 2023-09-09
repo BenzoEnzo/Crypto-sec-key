43e523676ad515671ec4637bc1e6e427f12e4813
KubaBart <256466@student.pwr.edu.pl>
2023-09-09 19:50:08 +0200

    Init WebService

---
+++ src/main/java/pl/bartus/benzo/enzo/cryptoseckey/CryptoSecKeyApplication.java
@@ -0,0 +1,13 @@
+package pl.bartus.benzo.enzo.cryptoseckey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoSecKeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoSecKeyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/bartus/benzo/enzo/cryptoseckey/CryptoSecKeyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bartus.benzo.enzo.cryptoseckey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoSecKeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
